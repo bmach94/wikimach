58ca6e2a86b0675464804159f78b624b0f517a22
Beata Mach <rasti.budyn@gmail.com>
2024-05-22 21:57:28 +0200

    Add gitignore - secret

--- Google/ang_with_marta/g_sheets.py
+++ Google/ang_with_marta/g_sheets.py
@@ -4,6 +4,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 class GcpConnect:
     
+    sheet_name = 'Gra w słówka'
+    
     # Ścieżka do pliku JSON z kluczami dostępu
     credentials_file = './angwithmarta.json'
 
@@ -15,22 +17,12 @@ class GcpConnect:
         credentials = ServiceAccountCredentials.from_json_keyfile_name(self.credentials_file, self.scope)
         return gspread.authorize(credentials) # return client
 
-class GcpAction(GcpConnect):
-        
-    # Otwórz arkusz Google Sheets za pomocą nazwy lub ID arkusza
-    spreadsheet_id = '1tHr0ppgv03PItKj5KPMx0k5tsyYbpXXEeO1LD3m3s_g'
-    sheet_name = 'Gra w słówka'
 
-
-#sheet = client.open_by_key(spreadsheet_id)#.worksheet(sheet_name)
-# Użyj poprawnej nazwy arkusza
-#worksheet_name = "Gra w słówka"
-
-gcp = GcpAction()
+gcp = GcpConnect()
 client = gcp.authorize()
 sheet = client.open(gcp.sheet_name).sheet1
 
 # get all
 plantas = sheet.get_all_values()
 
-print(plantas)
+print(type(plantas))
