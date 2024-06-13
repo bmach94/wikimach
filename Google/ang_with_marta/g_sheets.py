1ed2c9e82dde230cc65d6efa456aa7a05b4df163
Beata Mach <rasti.budyn@gmail.com>
2024-06-13 13:30:44 +0200

    Add Dictionary Game

--- Google/ang_with_marta/g_sheets.py
+++ Google/ang_with_marta/g_sheets.py
@@ -7,6 +7,8 @@ import yaml
 
 import time
 
+from random import *
+
 class Config():
     
     def loadConfig(self):
@@ -21,15 +23,14 @@ class GcpConnect(Config):
     
     def authorize(self):
         
-        #config = self.loadConfig()
         # Autoryzacja z użyciem danych uwierzytelniających
         credentials = ServiceAccountCredentials.from_json_keyfile_name(self.config['GcpConnect']['credentials_file'], self.config['GcpConnect']['scope'])
         return gspread.authorize(credentials) # return client
 
 class Xlsx(Config):
     
-    def __init__(self, gClient, columnNames = ['week','data','Beata','tłumaczenie','t1','Marta','tłumaczenie','t2']):
-        self._columnNames = columnNames
+    def __init__(self, gClient):
+        
         self.gclient = gClient
         self.config = self.loadConfig()
     # connect with sheet    
@@ -49,7 +50,28 @@ class Xlsx(Config):
         data = self.getAll(self.openSheet())
         return pd.DataFrame(data)
     
-
+    
+class Game(Config):
+    
+    def __init__(self):
+        self.config = self.loadConfig()
+        
+    def dictionaryGame(self, dictionary):
+        idx = self.config[user][0]
+        if user in self.config['Users']:
+            sizeDict = len(dictionary)
+            while(loop):
+                sRow = randrange(sizeDict)
+                if dictionary[sRow][idx]:
+                    print(dictionary[sRow][idx])
+                    time.sleep(self.config['dictionaryBreak'])
+                    if dictionary[sRow][idx+1:idx+3][0]:
+                        print(dictionary[sRow][idx+1:idx+3][0])
+                    elif dictionary[sRow][idx+1:idx+3][1]:
+                        print(dictionary[sRow][idx+1:idx+3][1])
+                    elif dictionary[sRow][idx+1:idx+3][0] == '':
+                        print('You need to translate on your own :).')
+                        continue
 # connect with GCP    
 client = GcpConnect().authorize()
 
@@ -68,20 +90,15 @@ while (loop):
     
     user = input()
     
-    val = int(input('Wybierz: \n1) Słownik - losuje słowko, po 5 s. wyswietla sie tłumaczenie\n2) Uzupelnienie zdan\nPodaj cyfre: '))
+    val = int(input(f"Wybierz: \n1) Słownik - losuje słowko, po {config['dictionaryBreak']} s. wyswietla sie tłumaczenie\n2) Uzupelnienie zdan\nPodaj cyfre: "))
     
     if val == 1:
         # DICTIONARY
         sheet2 = xlsx.openSheet(2) # get the second worksheet
         df2 = xlsx.getAll(sheet2)
-
-        dictionary = df2[1:]
-        if user in config['Users']:
-            
-            for sRow in range(5):
-                idx = config[user][0]
-                print(dictionary[sRow][idx])
-                time.sleep(1)
-                print(dictionary[sRow][idx+1:idx+3])
-                    
-    break
+        
+        Game().dictionaryGame(df2[1:])  # only dictionary words    
+    elif val == 2:
+        print('Game is not ready.')  
+    else:
+        break
